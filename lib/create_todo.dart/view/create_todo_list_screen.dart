4804d190ddd1ec7807e85905b08e0de07800be54
sairaj15 <sairajsawant2004@gmail.com>
2024-07-22 15:04:48 +0530

    few states are handled

--- lib/create_todo.dart/view/create_todo_list_screen.dart
+++ lib/create_todo.dart/view/create_todo_list_screen.dart
@@ -29,77 +29,95 @@ class _CreateTodoListScreenState extends State<CreateTodoListScreen> {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<CreateTodoBloc, CreateTodoState>(
-      builder: (context, state){
+      builder: (context, state) {
+        if (state.todoListStatus == CreateTodoStatus.success) {
+          context
+              .read<AllTodoListBloc>()
+              .add(const AllTodoListEventFetchEvent());
+          Navigator.pop(context);
+        }
+        if (state is CreateTodoStateAdd) {
+          const SnackBar(content: Text('Todo Created!'));
+        } else {
+          const SnackBar(content: Text("Todolist Updated"));
+        }
         return PopScope(
-          canPop: state.todoListStatus == CreateTodoStatus.loading ? false : true,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text(
-                widget.todoListModel == null ? 'Create Todo List' : 'Edit Todo List',
-              ),
-              centerTitle: true,
-            ),
-            floatingActionButton: FloatingActionButton.extended(
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  final title = titleController.text;
-                  final description = descriptionController.text;
-                  if (widget.todoListModel != null) {
-                    context.read<CreateTodoBloc>().add(
-                      CreateTodoEventUpdateEvent(todoListModel: widget.todoListModel!.copyWith(
-                        title: title,
-                        description: description,
-                      ))
-                    );
-                    context.read<CreateTodoBloc>().add(
-                      CreateTodoEventSaveEvent(
-                        title: title,
-                        description: description,
-                      )
-                    );
-                  }
-                }
-              }
-            , label: const Text("Save")),
-            body: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: titleController,
-                      style: Theme.of(context).textTheme.titleLarge,
-                      maxLength: 100,
-                      decoration: const InputDecoration(
-                        hintText: 'Title',
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter title';
-                        }
-                        return null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: descriptionController,
-                      maxLength: 500,
-                      maxLines: 5,
-                      style: Theme.of(context).textTheme.bodyLarge,
-                      decoration: const InputDecoration(
-                        hintText: 'Description',
-                      )
-                    )
-                  ],
+            canPop:
+                state.todoListStatus == CreateTodoStatus.loading ? false : true,
+            child: Stack(children: [
+              Scaffold(
+                appBar: AppBar(
+                  title: Text(
+                    widget.todoListModel == null
+                        ? 'Create Todo List'
+                        : 'Edit Todo List',
                   ),
+                  centerTitle: true,
+                ),
+                floatingActionButton: FloatingActionButton.extended(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        final title = titleController.text;
+                        final description = descriptionController.text;
+                        if (widget.todoListModel != null) {
+                          context
+                              .read<CreateTodoBloc>()
+                              .add(CreateTodoEventUpdateEvent(
+                                  todoListModel: widget.todoListModel!.copyWith(
+                                title: title,
+                                description: description,
+                              )));
+                        } else {
+                          context
+                              .read<CreateTodoBloc>()
+                              .add(CreateTodoEventSaveEvent(
+                                title: title,
+                                description: description,
+                              ));
+                        }
+                      }
+                    },
+                    label: const Text("Save")),
+                body: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: titleController,
+                          style: Theme.of(context).textTheme.titleLarge,
+                          maxLength: 100,
+                          decoration: const InputDecoration(
+                            hintText: 'Title',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter title';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                            controller: descriptionController,
+                            maxLength: 500,
+                            maxLines: 5,
+                            style: Theme.of(context).textTheme.bodyLarge,
+                            decoration: const InputDecoration(
+                              hintText: 'Description',
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            ),
-          )
-          );
-      }, 
-      listener: (context, state){
-        if(state.todoListStatus == CreateTodoStatus.success){
-          context.read<AllTodoListBloc>().add(const AllTodoListEventFetchEvent());
+            ]));
+      },
+      listener: (context, state) {
+        if (state.todoListStatus == CreateTodoStatus.success) {
+          context
+              .read<AllTodoListBloc>()
+              .add(const AllTodoListEventFetchEvent());
           Navigator.of(context).pop();
           log("Todo created");
         }
@@ -114,13 +132,11 @@ class _CreateTodoListScreenState extends State<CreateTodoListScreen> {
               ),
               SizedBox(height: 20),
               Material(
-                color: Colors.transparent,
-                child: Text("Creating Todo......")
-              )
+                  color: Colors.transparent, child: Text("Creating Todo......"))
             ],
           );
         }
       },
-      );
+    );
   }
 }
